9f60e48bbb4f6297f9336a479ef994fed1f91f95
Norton Wang <1502552+FloorLamp@users.noreply.github.com>
2021-06-05 22:11:15 +0700

    Create didc-js

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use candid::{check_prog, IDLProg, TypeEnv};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Bindings {
+    js: String,
+    ts: String,
+    motoko: String,
+}
+
+#[wasm_bindgen]
+impl Bindings {
+    #[wasm_bindgen(getter)]
+    pub fn js(&self) -> String {
+        self.js.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn ts(&self) -> String {
+        self.ts.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn motoko(&self) -> String {
+        self.motoko.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn generate(prog: &str) -> Option<Bindings> {
+    let ast = prog.parse::<IDLProg>().ok()?;
+    let mut env = TypeEnv::new();
+    let actor = check_prog(&mut env, &ast).ok()?;
+    Some(Bindings {
+        js: candid::bindings::javascript::compile(&env, &actor),
+        ts: candid::bindings::typescript::compile(&env, &actor),
+        motoko: candid::bindings::motoko::compile(&env, &actor),
+    })
+}
